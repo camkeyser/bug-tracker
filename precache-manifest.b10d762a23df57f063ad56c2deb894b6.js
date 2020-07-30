413f8e8e06c36023eb838534543efff74ab8c643
camkeyser <camkeyser@live.com>
2020-07-30 18:10:17 +0100

    Updates

---
+++ precache-manifest.b10d762a23df57f063ad56c2deb894b6.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f34675a1d9af949a4b22943b21f34c1a",
+    "url": "/bug-tracker/index.html"
+  },
+  {
+    "revision": "375333cd49da3e44b9dd",
+    "url": "/bug-tracker/static/css/main.09ba4e53.chunk.css"
+  },
+  {
+    "revision": "e529ad3bc164ce7415f6",
+    "url": "/bug-tracker/static/js/2.7c69c077.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/bug-tracker/static/js/2.7c69c077.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "375333cd49da3e44b9dd",
+    "url": "/bug-tracker/static/js/main.311d900f.chunk.js"
+  },
+  {
+    "revision": "a31a8ef397a4668de8f0",
+    "url": "/bug-tracker/static/js/runtime-main.890cf13e.js"
+  },
+  {
+    "revision": "e9832a69425028fafe1d6205e648f5d9",
+    "url": "/bug-tracker/static/media/add.e9832a69.png"
+  },
+  {
+    "revision": "b0db2461a2b2525e331af8f1f04977fe",
+    "url": "/bug-tracker/static/media/alert.b0db2461.png"
+  },
+  {
+    "revision": "35aaebaa72968946655fae4682cda755",
+    "url": "/bug-tracker/static/media/avatar.35aaebaa.png"
+  },
+  {
+    "revision": "5ed6312ced7e3a5a84a81412e2d8b114",
+    "url": "/bug-tracker/static/media/close.5ed6312c.png"
+  },
+  {
+    "revision": "16c7f94bd3ea3cd7cdd301e37e91f592",
+    "url": "/bug-tracker/static/media/dashboard.16c7f94b.png"
+  },
+  {
+    "revision": "1111a0481bdca1d96e56af7a353b86da",
+    "url": "/bug-tracker/static/media/delete.1111a048.png"
+  },
+  {
+    "revision": "6a16828406106e57ed472a2ebdcd2ff3",
+    "url": "/bug-tracker/static/media/dots.6a168284.png"
+  },
+  {
+    "revision": "4e842f7645e382a4ac2bb7c0dd018ff2",
+    "url": "/bug-tracker/static/media/filter.4e842f76.png"
+  },
+  {
+    "revision": "03d98bc2f3a4e6dc160d55adb45feca3",
+    "url": "/bug-tracker/static/media/logo.03d98bc2.png"
+  },
+  {
+    "revision": "33047336aa1a768763a267aa650a9814",
+    "url": "/bug-tracker/static/media/news.33047336.png"
+  },
+  {
+    "revision": "3d73387c2a1e21f207a8357724f536ba",
+    "url": "/bug-tracker/static/media/owfont-regular.3d73387c.ttf"
+  },
+  {
+    "revision": "e00c269cd4fb37b24eea2e4f37982c3d",
+    "url": "/bug-tracker/static/media/owfont-regular.e00c269c.eot"
+  },
+  {
+    "revision": "ed668bf0b123371be46d005b428f6188",
+    "url": "/bug-tracker/static/media/owfont-regular.ed668bf0.woff"
+  },
+  {
+    "revision": "f3ce0ef136eaaf2b1db22cf4069cb5dc",
+    "url": "/bug-tracker/static/media/owfont-regular.f3ce0ef1.svg"
+  },
+  {
+    "revision": "5f6c07bba36e0e5a28bd9fa2dbe80b6a",
+    "url": "/bug-tracker/static/media/sadness.5f6c07bb.png"
+  },
+  {
+    "revision": "aa0872125d37a1950a7ab1e3d3f55c4c",
+    "url": "/bug-tracker/static/media/search.aa087212.png"
+  },
+  {
+    "revision": "290b2a9e6913324020c0e38c0ae4f034",
+    "url": "/bug-tracker/static/media/tickets.290b2a9e.png"
+  },
+  {
+    "revision": "900843d695f26ca1508fdfe73c63f5b1",
+    "url": "/bug-tracker/static/media/tools.900843d6.png"
+  }
+]);
